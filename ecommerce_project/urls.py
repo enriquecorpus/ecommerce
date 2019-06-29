5bb841d94c136687c9bf4401468677a400ec8094
Enrique Corpus <enriquecorpus@skydev.solutions>
2019-06-29 18:15:20 +0800

    Set up template Hello World

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+import django.contrib.admin
+import django.views.generic
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', django.contrib.admin.site.urls),
+    path('', django.views.generic.TemplateView.as_view(template_name='ecommerce/index.html'),
+         name='home'),
+    # path('home/', include(('home.urls ','home'), namespace='home')),
 ]
